d00b295e3487dd6edd2061991ce7da4891971635
Adarsh <adarsh.mammen@gmail.com>
2016-08-13 14:41:52 -0400

    added model training

---
+++ Model_Training/practice.py
@@ -0,0 +1,55 @@
+import os
+from clarifai.rest import ApiClient, Image
+
+
+s = '/Users/adarsh/Desktop/testAI/hand_open/'
+label = 'hand_open'
+li_hand_open = []
+os.chdir('/Users/adarsh/Desktop/testAI/hand_open/')
+for i in os.listdir(os.getcwd()):
+	if i.endswith(".jpg"):
+	    li_hand_open.append(Image(file_obj=open(s+i),labels=[label]))
+
+
+s = '/Users/adarsh/Desktop/testAI/hand_closed/'
+label = 'hand_closed'
+li_hand_closed = []
+os.chdir('/Users/adarsh/Desktop/testAI/hand_closed/')
+for i in os.listdir(os.getcwd()):
+	if i.endswith(".jpg"):
+	    li_hand_closed.append(Image(file_obj=open(s+i),labels=[label]))
+
+
+
+s = '/Users/adarsh/Desktop/testAI/hand_crossed/'
+label = 'hand_crossed'
+li_hand_crossed = []
+os.chdir('/Users/adarsh/Desktop/testAI/hand_crossed/')
+for i in os.listdir(os.getcwd()):
+	if i.endswith(".jpg"):
+	    li_hand_crossed.append(Image(file_obj=open(s+i),labels=[label]))
+
+
+input_list = []
+input_list.extend(li_hand_open)
+input_list.extend(li_hand_closed)
+input_list.extend(li_hand_crossed)
+
+
+api.addInputs(input_list)
+
+res = api.createModel(model_name='Gestures', concept_ids=['hand_open','hand_closed','hand_crossed'])
+
+model_id = res['model']['id']
+
+#### TRAINING MODEL 
+
+api.trainModel(model_id=model_id)
+
+print model_id
+
+
+
+
+
+
---
+++ Model_Training/readme_first.txt
@@ -0,0 +1,4 @@
+
+
+Make sure you change the path names before training (especially if you pulled from Git
+)
